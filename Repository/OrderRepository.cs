8d55ec05a8a057d4e764bbac1a8e99be4f17ff98
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-02-01 21:39:15 +0200

    Finishing Repositories

--- Repository/OrderRepository.cs
+++ Repository/OrderRepository.cs
@@ -16,77 +16,21 @@ public class OrdersRepository<T> : IOrderRepository<T> where T : Orders
         _context = context;
         entities = context.Set<T>();
     }
-    public T AddCoupon(T couponModel)
+    public T CreateOrder(T Model)
     {
-        if (couponModel != null)
+        if (Model != null)
         {
-            entities.Add(couponModel);
+            entities.Add(Model);
             _context.SaveChangesAsync();
-            return couponModel;
+            return Model;
         }
         else
         {
             throw new Exception("There's something wrong in your Input try again");
         }
-
     }
-    public async Task<T> UpdateCoupon(string id, T couponModel)
-    {
-        var coupon = await entities.FindAsync(id);
-        if (coupon != null)
-        {
-            //coupon.Code = couponModel.Code;
-            //coupon.DiscountType = couponModel.DiscountType;
-            //coupon.MaxUses = couponModel.MaxUses;
-            //coupon.Uses = couponModel.Uses;
-            //coupon.IsEnabled = couponModel.IsEnabled;
-            //coupon.Value = couponModel.Value;
-            await _context.SaveChangesAsync();
-            return coupon;
-        }
-        else
-        {
-            throw new Exception("Coupon not found");
-        }
-    }
-    public async Task<bool> DeleteCoupon(string id)
-    {
-        var coupon = await entities.FindAsync(id);
-        if (coupon != null)
-        {
-            entities.Remove(coupon);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-    }
-
-    public async Task<bool> DeactivateCoupon(string id)
-    {
-        var coupon = await entities.FindAsync(id);
-        if (coupon != null)
-        {
-            //coupon.IsEnabled = false;
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public T CreateOrder(T couponModel)
-    {
-        throw new NotImplementedException();
-    }
-
     public IEnumerable<T> GetOrdersHistory(string symbol)
     {
-        throw new NotImplementedException();
+        return entities.Where(orders => orders.StockSymbol == symbol);
     }
 }
--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -21,59 +21,14 @@ namespace Repository
             _context = context;
             entities = context.Set<T>();
         }
-
-        public T AddAppointment(T Model)
-        {
-            if (Model == null)
-            {
-                throw new ArgumentNullException("entity");
-            }
-            entities.Add(Model);
-            _context.SaveChanges();
-            return Model;
-        }
-        public async Task<bool> DeleteAppointmentById(string id)
-        {
-            var Appointment = await entities.FindAsync(id);
-            if (Appointment != null)
-            {
-                entities.Remove(Appointment);
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
         public IEnumerable<T> GetStockData()
         {
-            throw new NotImplementedException();
+        
         }
 
-        public IEnumerable<T> GetStockHistory(string Symbol)
+        public IEnumerable<T> GetStockHistory(string symbol)
         {
-            throw new NotImplementedException();
-        }
-
-        public async Task<T> UpdateAppointmentById(string id, T Model)
-        {
-            var Appointment = await entities.FindAsync(id);
-            if (Appointment != null)
-            {
-                //Appointment.Time = Model.Time;
-                //Appointment.Date = Model.Date;
-                //Appointment.Price = Model.Price;
-                //Appointment.Days = Model.Days;
-                //Appointment.Date = Model.Date;
-                await _context.SaveChangesAsync();
-                return Appointment;
-            }
-            else
-            {
-                throw new Exception("Coupon not found");
-            }
+            return entities.Where(stock => stock.StockSymbol == symbol);
         }
     }
 }
